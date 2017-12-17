594aeb50d86717f623b5ced65c4a667425ca1a4a
Lucas Vieira <lucasvieira@protonmail.com>
2017-12-17 00:27:47 -0200

    Using portuguese on the wallet is simply not worth it

--- internetz/src/main.rs
+++ internetz/src/main.rs
@@ -5,13 +5,13 @@ use rustyline::Editor;
 
 const INTERNETZ_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 static COMMANDS: &'static str =
-    "sair  -- Sai da aplicação.\n\
-     ajuda -- Mostra esta tabela.\n";
+    "quit  -- Quits application.\n\
+     help  -- Shows this help prompt.\n";
 
 
 fn main() {
     println!("internetz {}", INTERNETZ_VERSION);
-    println!("Carteira CLI para lulzcoin, a moeda inimaginavelmente inútil.");
+    println!("CLI wallet for lulzcoin, the unimaginably useless cryptocoin.");
     println!("Copyright (C) 2017 Lucas Vieira.");
     println!("This program is distributed under the MIT License. Check source code for details.");
     
@@ -20,10 +20,10 @@ fn main() {
 
     // Load REPL history
     if let Err(_) = rl.load_history(".internetz-history") {
-        println!("Nenhum comando no histórico.");
+        println!("No command on history.");
     }
 
-    println!("Para uma lista de comandos, digite `ajuda`.");
+    println!("For a list of commands, type `help`.");
     loop {
         let readline = rl.readline("INTERNETZ > ");
         match readline {
@@ -38,22 +38,25 @@ fn main() {
                     let args = &atoms[1..];
 
                     match command.as_ref() {
-                        "sair"  => break,
-                        "ajuda" => println!("LISTA DE COMANDOS:\n{}", COMMANDS),
+                        "quit" | "exit"  => break,
+                        "help" => {
+                            println!("You're gonna get paid in INTERNETZ: Worth more than a bar of lulz.");
+                            println!("COMMAND LIST:\n{}", COMMANDS);
+                        },
                         _       => println!("Nao implementado"),
                     }
                 }
             },
             Err(ReadlineError::Interrupted) => {
-                println!("Recebido um sinal de interrupção. Saindo.");
+                println!("Interrupt received. Quitting.");
                 break;
             },
             Err(ReadlineError::Eof) => {
-                println!("Recebido um sinal de fim de arquivo. Saindo.");
+                println!("EOF received. Quitting.");
                 break;
             },
             Err(err) => {
-                println!("Erro de leitura de linha: {:?}. Saindo.", err);
+                println!("Error reading line: {:?}. Quitting.", err);
                 break;
             },
         }
