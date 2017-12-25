cbaf496340f8bb604db7f2d8979bdd21b5272142
Lucas Vieira <lucasvieira@protonmail.com>
2017-12-25 14:58:07 -0200

    Nice changes to wallet software.

--- internetz/src/main.rs
+++ internetz/src/main.rs
@@ -1,12 +1,32 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+extern crate crypto;
+extern crate openssl;
+extern crate pem;
+extern crate rust_base58;
+extern crate rpassword;
+
 extern crate rustyline;
+
+
+
+pub mod wallet;
+use wallet::Wallet;
+
+
 
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
+use std::fs;
 
 const INTERNETZ_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 static COMMANDS: &'static str =
-    "quit  -- Quits application.\n\
-     help  -- Shows this help prompt.\n";
+    "quit   -- Quits application.\n\
+     create -- [WIP] Generates a new wallet.\n\
+     open   -- [PLANNED] Opens a wallet.\n\
+     help   -- Shows this help prompt.\n";
 
 
 fn main() {
@@ -14,6 +34,14 @@ fn main() {
     println!("CLI wallet for lulzcoin, the unimaginably useless cryptocoin.");
     println!("Copyright (C) 2017 Lucas Vieira.");
     println!("This program is distributed under the MIT License. Check source code for details.");
+
+    // Ensure lulzcoin dir exists
+    let _ = fs::create_dir_all("~/.lulzcoin");
+
+    // Load wallet data
+    println!("THIS IS A BETA! Expect bugs here and there on this wallet. Plus, please, do not");
+    println!("consider this a safe software; even though there are some safety measures implemented,");
+    println!("this software still cannot hold your money safely.");
     
     // Editor
     let mut rl = Editor::<()>::new();
@@ -42,6 +70,34 @@ fn main() {
                         "help" => {
                             println!("You're gonna get paid in INTERNETZ: Worth more than a bar of lulz.");
                             println!("COMMAND LIST:\n{}", COMMANDS);
+                        },
+                        "create" => {
+                            // Retrieve name for wallet
+                            let walletname = rl.readline("Please input a wallet name: ").unwrap();
+                            let filename = String::from("~/.lulzcoin/") + walletname.as_ref();
+                            let filename = filename + ".lulz";
+                            
+                            println!("Your wallet will temporarily be stored in ~/.lulzcoin/{}.lulz.",
+                                     walletname);
+
+                            // Wallet test
+                            match Wallet::new() {
+                                Ok(wallet) => {
+                                    println!("Wallet successfully generated. Your addresses:");
+                                    for addr in &wallet.addresses {
+                                        println!("\t{}", addr);
+                                    }
+                                    match wallet.save(filename.as_ref()) {
+                                        Ok(_) => println!("Wallet saved in {}.", filename),
+                                        Err(error) => println!("Error saving wallet: {}", error),
+                                    }
+                                    
+                                },
+                                Err(error) => {
+                                    println!("Error generating wallet: {}", error);
+                                },
+                            }
+                            
                         },
                         _ => println!("Nao implementado"),
                     }
---
+++ internetz/src/wallet/mod.rs
@@ -0,0 +1,130 @@
+use serde_json;
+use openssl::rsa::Rsa;
+use pem::{Pem, encode};
+use crypto::sha2::Sha256;
+use crypto::ripemd160::Ripemd160;
+use crypto::digest::Digest;
+use rust_base58::{ToBase58, FromBase58};
+use rpassword;
+use std::io::Write;
+use std::fs::File;
+
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Wallet {
+    pub addresses:   Vec<String>,
+    pub balances:    Vec<i64>,
+    pub last_height: usize,
+    pub pubkeys:     Vec<Vec<u8>>,
+    privkeys:        Vec<String>, // REALLY REALLY REALLY BAD IDEA. MUST REPLACE.
+}
+
+impl Wallet {
+    pub fn new() -> Result<Wallet, &'static str> {
+        
+        let mut wallet = Wallet {
+            addresses:   vec![],
+            balances:    vec![],
+            last_height: 0,
+            pubkeys:     vec![],
+            privkeys:    vec![],
+        };
+
+        // I need to store this encrypted in a persistent storage!
+        let mut privkeys = vec![];
+
+        println!("Generating public/private keypairs...");
+        for i in 0..10 {
+            // Generate keypair
+            //println!("Generating keypair #{}...", i+1);
+            {
+                let rsa = Rsa::generate(4096).unwrap();
+                wallet.pubkeys.push(rsa.public_key_to_der().unwrap().clone());
+                privkeys.push(rsa.private_key_to_der().unwrap().clone());
+
+                let private_pem = Pem {
+                    tag: String::from("RSA PRIVKEY"),
+                    contents: privkeys[i].clone(),
+                };
+                let privkey = encode(&private_pem);
+                wallet.privkeys.push(privkey.clone()); // TODO: Move to persistent storage
+            }
+            // Generate binary addresss
+            let binaddr = Wallet::generate_binaddr(&wallet.pubkeys[i]);
+            wallet.addresses.push(Wallet::generate_address(&binaddr));
+            wallet.balances.push(0);
+        }
+
+        // Input password to encrypt and save wallet privkeys to persistent
+        // storage
+        {
+            let password = rpassword::prompt_password_stdout("Input encryption passphrase: ").unwrap();
+            if password == rpassword::prompt_password_stdout("Please enter passphrase again: ").unwrap() {
+                //println!("Password is correct. However, we're not using it, I'm truly sorry");
+                Ok(wallet)
+            }
+            else {
+                Err("Passphrases did not match!")
+            }
+        }
+    }
+
+    pub fn generate_binaddr(pubkey: &Vec<u8>) -> String {
+        let sha256digest = {
+            let mut hasher = Sha256::new();
+            hasher.input(pubkey);
+            hasher.result_str()
+        };
+
+        let ripemd160digest = {
+            let mut hasher = Ripemd160::new();
+            hasher.input(&sha256digest.into_bytes());
+            hasher.result_str()
+        };
+        let ripemd160digest = "00".to_owned() + ripemd160digest.as_ref();
+
+        let sha256digest2n3 = {
+            let mut hasher1 = Sha256::new();
+            let mut hasher2 = Sha256::new();
+            hasher1.input(&ripemd160digest.clone().into_bytes());
+            hasher2.input(&hasher1.result_str().into_bytes());
+            hasher2.result_str()
+        };
+
+        let checksum = String::from(&sha256digest2n3[..8]);
+        checksum + ripemd160digest.as_ref()
+    }
+
+    pub fn generate_address(binaddr: &String) -> String {
+        assert_eq!(binaddr.len(), 50);
+        
+        let mut binvec = vec![];
+        for i in 0..25 {
+            let hex = &binaddr[(i*2)..(i*2)+2];
+            binvec.push(i64::from_str_radix(hex, 16).unwrap() as u8);
+        }
+
+        binvec.to_base58()
+    }
+
+    pub fn save(&self, filename: &str) -> Result<(), &'static str> {
+        // We first serialize our wallet to json.
+        match serde_json::to_string(&self) {
+            Ok(serialized) => {
+                // TODO: we should encrypt stuff here
+                // Now we open our file
+                let f = File::create(filename);
+                match f {
+                    Ok(mut f) => {
+                        match f.write_all(serialized.as_bytes()) {
+                            Ok(_) => Ok(()),
+                            _ => Err("Unable to write file.")
+                        }
+                    },
+                    _ => Err("Error opening file.")
+                }
+            },
+            _ => Err("Error serializing wallet.")
+        }
+    }
+}
